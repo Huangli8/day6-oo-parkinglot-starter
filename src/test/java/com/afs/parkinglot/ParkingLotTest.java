64c63cc3741fc6eca8eb4553acbd4d88d64a7b3a
wcpbbl <1353078781@qq.com>
2025-09-08 21:28:44 +0800

    story 1 case 2

--- src/test/java/com/afs/parkinglot/ParkingLotTest.java
+++ src/test/java/com/afs/parkinglot/ParkingLotTest.java
@@ -2,16 +2,25 @@ package com.afs.parkinglot;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class ParkingLotTest {
     @Test
-    void should_return_parking_ticket_when_given_car_parked(){
+    void should_return_parking_ticket_when_park_a_car(){
         Car car=new Car();
         ParkingLot parkingLot=new ParkingLot();
         ParkingTicket parkingTicket=parkingLot.park(car);
         assertNotNull(parkingTicket);
     }
 
+    @Test
+    void should_return_parked_car_when_fetch_a_car_given_parking_ticket(){
+        Car car=new Car();
+        ParkingLot parkingLot=new ParkingLot();
+        ParkingTicket parkingTicket=parkingLot.park(car);
+        assertEquals(car,parkingLot.fetch(parkingTicket));
+
+    }
 
 }
